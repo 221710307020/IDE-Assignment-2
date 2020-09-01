f581148f2a0f63bb3238c681051ceac2bf254853
221710307020 <68814847+221710307020@users.noreply.github.com>
2020-09-01 09:28:30 +0530

    Create StudentInfo.java

---
+++ StudentInfo.java
@@ -0,0 +1,11 @@
+package assignment1;
+
+public class studentinfo {
+
+	public static void main(String[] args) {
+		System.out.println("Student Name : Vidya");
+		System.out.println("Student Rollno. : 221710307020");
+		System.out.println("Student Section : B7");
+	}
+
+}
